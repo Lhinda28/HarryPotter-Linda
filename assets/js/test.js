7c5208bd119075e8f9374b4471c083444f81c3a2
Linda Loza <lindaloza8@gmail.com>
2023-07-12 22:06:25 -0600

    CREACION DEL PORTAL HP

---
+++ assets/js/test.js
@@ -0,0 +1,31 @@
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://hp-api.onrender.com/api/characters/students");
+xhr.send();
+
+xhr.responseType="json";
+xhr.onload=()=> {
+    console.log(xhr);
+    if (xhr.readyState == 4 && xhr.status==200){
+        const data =xhr.response;
+        console.log(data);
+        var studentList = data.map(x => x.name);
+        console.log(studentList);
+        var content = document.getElementById('lindaDiv');
+        // Crea el elemento <ul> (lista desordenada)
+        var ul = document.createElement("ul");
+
+        // Recorre el array y crea un <li> (elemento de lista) por cada elemento
+        for (var i = 0; i < studentList.length; i++) {
+            var li = document.createElement("li");
+            li.appendChild(document.createTextNode(studentList[i]));
+            ul.appendChild(li);
+        }
+
+        // Agrega la lista <ul> al elemento <div>
+        content.appendChild(ul);
+
+    }else{
+        console.log(`Error: ${xrh.status}`)
+    }
+}
